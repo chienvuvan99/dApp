b4888206b69ecf2da10575356c8d92077e3b722b
chien.vu <abc@abcs-iMac.local>
2022-04-21 17:33:20 +0700

    happy coding

--- frontend/screen/home/index.tsx
+++ frontend/screen/home/index.tsx
@@ -1,16 +1,69 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, View, Button, Image, Pressable} from 'react-native';
+import React, {useRef, useEffect, useState} from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import tw from 'twrnc';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+  Extrapolate,
+  withRepeat,
+  withDelay,
+  Easing,
+} from 'react-native-reanimated';
+
+const Pulse = ({delay = 0, repeat}) => {
+  const animation = useSharedValue(0);
+  useEffect(() => {
+    animation.value = withDelay(
+      delay,
+      withRepeat(
+        withTiming(1, {
+          duration: 2000,
+          easing: Easing.linear,
+        }),
+        repeat ? -1 : 1,
+        false,
+      ),
+    );
+  }, []);
+  const animatedStyles = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      animation.value,
+      [0, 1],
+      [0.6, 0],
+      Extrapolate.CLAMP,
+    );
+    return {
+      opacity: opacity,
+      transform: [{scale: animation.value}],
+    };
+  });
+  return <Animated.View style={[styles.circle, animatedStyles]} />;
+};
+
 const HomeScreen = () => {
+  const [pulse, setPulse] = useState([1]);
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <View>
-        <View
-          style={tw`w-31 h-31 bg-[#17D85C] justify-center items-center rounded-100`}
+    <View style={styles.container}>
+      <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <Pressable
+          style={styles.innerCircle}
+          onPress={() => {
+            setPulse(prev => [...prev, Math.random()]);
+          }}
         >
-          <Ionicons name="checkmark-sharp" size={70} color={'white'} />
-        </View>
+          <Image
+            style={styles.innerCircle}
+            source={{
+              uri: 'https://reactnative.dev/img/tiny_logo.png',
+            }}
+          />
+        </Pressable>
+        {pulse.map((item, index) => (
+          <Pulse repeat={index === 0} />
+        ))}
       </View>
     </View>
   );
@@ -18,4 +71,29 @@ const HomeScreen = () => {
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: 50,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  circle: {
+    width: 300,
+    borderRadius: 150,
+    height: 300,
+    position: 'absolute',
+    borderColor: '#e91e63',
+    borderWidth: 4,
+    backgroundColor: '#ff6090',
+  },
+  innerCircle: {
+    width: 80,
+    borderRadius: 40,
+    height: 80,
+    zIndex: 100,
+    position: 'absolute',
+    backgroundColor: 'white',
+  },
+});
